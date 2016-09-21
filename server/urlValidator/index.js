7eec76108db29730924559c45ef1daa63b0990a9
Фёдор Плеханов < >
2016-09-21 20:13:55 +0300

    edit validator. delete pathname from mini funcs

--- server/urlValidator/index.js
+++ server/urlValidator/index.js
@@ -9,6 +9,7 @@ var GetBooks = "/GetBooks";
 var GetAuthors = "/GetAuthors";
 
 var validators = {};
+validators[MainPage] = MainPageValidate;
 validators[GetBook] =  BookValidate;
 validators[GetAuthor] = AuthorValidate;
 validators[GetBooks] = AuthorsValidate;
@@ -21,56 +22,27 @@ function GetFirstElem(pathname)
     return pathname.substr(0, pos+1);
 }
 
-function ValidateUrl(req,res)
-{
-    try {
-        var pathname = url.parse(req.url).pathname;
-        logger.WriteToLog("Get request whith pathname = " + pathname);
-        
-        if(pathname==="/") GetStartPage(res);
-        else {
-            if(pathname!=""&&pathname.length!=1) pathname = pathname.substr(1,pathname.length-1);
-            else throw "Error in pathname";
-            var pathFirstElem = GetFirstElem(pathname);
-            try {
-                validators[pathFirstElem](req, res, pathname);
-            } catch (error) {
-                throw "Bad query";
-            }
-        }
-    }
-    catch (ex)
-    {
-        res.writeHead(400, {'Content-Type': 'text/html'});
-        res.end(ex.message);
-    }
-
-}
-
-function GetStartPage(res)
+function MainPageValidate(req, res)
 {
     files.GetMainPage(res);
 }
 
-
-//validators for urls
-
-function BookValidate(req, res, pathname)
+function BookValidate(req, res)
 {
 
 }
 
-function AuthorValidate(req, res, pathname)
+function AuthorValidate(req, res)
 {
 
 }
 
-function AuthorsValidate(req, res, pathname)
+function AuthorsValidate(req, res)
 {
 
 }
 
-function BooksValidate(req, res, pathname)
+function BooksValidate(req, res)
 {
     logger.WriteToLog("Возврат книг");
     var books = models.GetAllBooks();
@@ -78,4 +50,16 @@ function BooksValidate(req, res, pathname)
     res.end(JSON.stringify(books));
 }
 
-exports.ValidateUrl = ValidateUrl;
+exports.ValidateUrl = function (req,res)
+{
+    try {
+        var pathname = url.parse(req.url).pathname;
+        logger.WriteToLog("Get request whith pathname = " + pathname);
+        validators[pathname](req, res);
+    }
+    catch (ex)
+    {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end(ex.message);
+    }
+}
